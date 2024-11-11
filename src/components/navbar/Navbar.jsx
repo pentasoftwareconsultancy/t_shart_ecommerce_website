f6f6c357175fffe991be75bcb33dbbe1c4238eb9
sonu2404 <sonaliekhande12@gmail.com>
2024-11-11 17:47:27 +0530

    first commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
-function Navbar() {
+import React, {useState} from 'react'
+import styles from './Navbar.module.css'
+import { CiSearch } from "react-icons/ci";
+function Navbar(){
+  const [isSearchBarOpen, setSearchBarOpen] = useState(false);
+  const toggleSearchBar = () => {
+    setSearchBarOpen(!isSearchBarOpen);
+  };
   return (
     <div>
-      
+      <div className={styles.Navbar}>
+      <a>Home </a>
+      <a>About Us</a>
+      <a>Collections</a>
+      <a>T-shirts</a>
+      <a>Hats</a>
+      <a>Contact</a>
+      <div className={styles.search}>
+          <CiSearch
+            size={20}
+            style={{ cursor: "pointer" }}
+            onClick={toggleSearchBar}
+          />
+          {isSearchBarOpen && (
+            <input
+              type="text"
+              placeholder="Search..."
+              style={{
+                marginLeft: "10px",
+                padding: "8px",
+                width: "200px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+              }}
+            />
+          )}
+        </div>
+      </div>
     </div>
   )
 }
