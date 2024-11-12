fc9e66d87462f9980883c418db898cbedd02111a
sonu2404 <sonaliekhande12@gmail.com>
2024-11-12 16:57:28 +0530

    change in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import styles from './Navbar.module.css'
 import { CiSearch } from "react-icons/ci";
+import { MdAccountCircle } from "react-icons/md";
+import { BsBagHeartFill } from "react-icons/bs";
+import { FaCartShopping } from "react-icons/fa6";
 function Navbar(){
   const [isSearchBarOpen, setSearchBarOpen] = useState(false);
   const toggleSearchBar = () => {
@@ -9,16 +12,26 @@ function Navbar(){
   return (
     <div>
       <div className={styles.Navbar}>
+        <a className={styles.logo}>Logo</a>
       <a>Home </a>
       <a>About Us</a>
-      <a>Collections</a>
-      <a>T-shirts</a>
+      <div className={styles.dropdown}>
+      <a className={styles.dropbtn}>Collections
+       </a>
+      <div className={styles.dropdowncontent}>
+        <a>Shirts</a>
+        <a>Hats</a>
+      </div>
+      </div>
+      <a>Shirts</a>
       <a>Hats</a>
       <a>Contact</a>
-      <div className={styles.search}>
+      <a className={styles.search}>
           <CiSearch
-            size={20}
-            style={{ cursor: "pointer" }}
+            size={25}
+            style={{ cursor: "pointer",
+             
+             }}
             onClick={toggleSearchBar}
           />
           {isSearchBarOpen && (
@@ -26,15 +39,31 @@ function Navbar(){
               type="text"
               placeholder="Search..."
               style={{
-                marginLeft: "-200px",
-                padding: "8px",
+                marginLeft: "-250px",
+                padding: "9px",
+                margintop:"-30px",
                 width: "200px",
                 border: "1px solid #ccc",
-                borderRadius: "4px",
+                borderRadius: "6px",
+               
               }}
             />
           )}
+        </a>
+        <div className={styles.login}>
+         
+          <a><BsBagHeartFill /></a>
+          <a><FaCartShopping /></a>
+           <div className={styles.dropdown}>
+           <a className={styles.searchs}><MdAccountCircle /></a>
+           <div className={styles.dropdowncontent}>
+            <a><MdAccountCircle />Login</a>
+        <a><BsBagHeartFill />Bag</a>
+        <a><FaCartShopping />Cart</a>
+      </div>
+           </div>
         </div>
+        {/* <div className={styles.logout}>Log Out</div> */}
       </div>
     </div>
   )
