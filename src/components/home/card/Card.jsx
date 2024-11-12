913efb7c197e87da5765ba406e2e11cba649fbf7
bhagyashree-garakate <bhagyashreed1709@gmail.com>
2024-11-12 05:33:11 +0530

    images added in hero and card component

--- src/components/home/card/Card.jsx
+++ src/components/home/card/Card.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './Card.module.css'; // Importing the CSS module for styles
+import image1 from './logoimg/logo1.jpeg';
+import image2 from './logoimg/logo2.jpeg';
+import image3 from './logoimg/logo3.jpeg';
 
 const Card = ({ id, image, name, price, onClick }) => {
   return (
@@ -30,70 +33,70 @@ const CardsGrid = () => {
       {/* Manually rendering 10 cards */}
       <Card
         id="card-1"
-        image="logo2.jpeg"
+        image= {image1}
         name="Brand Logo 1"
         price="$100.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-2"
-        image="logo3.jpeg"
+        image={image2}
         name="Brand Logo 2"
         price="$200.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-3"
-        image="logo1.jpeg"
+        image={image3}
         name="Brand Logo 3"
         price="$300.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-4"
-        image="logo2.jpeg"
+        image={image3}
         name="Brand Logo 4"
         price="$400.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-5"
-        image="logo3.jpeg"
+        image={image2}
         name="Brand Logo 5"
         price="$500.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-6"
-        image="logo2.jpeg"
+        image={image1}
         name="Brand Logo 6"
         price="$600.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-7"
-        image="logo1.jpeg"
+        image={image3}
         name="Brand Logo 7"
         price="$700.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-8"
-        image="logo3.jpeg"
+        image={image2}
         name="Brand Logo 8"
         price="$800.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-9"
-        image="logo2.jpeg"
+        image={image3}
         name="Brand Logo 9"
         price="$900.00"
         onClick={handleImageClick}
       />
       <Card
         id="card-10"
-        image="logo1.jpeg"
+        image={image1}
         name="Brand Logo 10"
         price="$1000.00"
         onClick={handleImageClick}
