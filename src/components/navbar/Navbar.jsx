d7842c09c604b9f238e8d533210c4ca9e4625a24
bhagyashree-garakate <bhagyashreed1709@gmail.com>
2024-12-02 02:51:08 +0530

    Change in Navbar design and Hero Section is done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,102 +20,99 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      {/* Logo */}
-      <div className={styles.logo}>
-        <Link to="/">Logo</Link>
-      </div>
-
-      {/* Navigation Links */}
-      <ul className={`${styles.navLinks} ${menuOpen ? styles.active : ""}`}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/aboutus">About</Link>
-        </li>
-        <li
-          className={`${styles.dropdown} ${collectionsOpen ? styles.active : ""}`}
-        >
-          <Link
-            to="#"
-            className={styles.dropdownToggle}
-            onClick={(e) => {
-              e.preventDefault();
-              toggleCollections();
-            }}
-          >
-            Collections
-          </Link>
-          {collectionsOpen && (
-            <ul className={styles.dropdownMenu}>
-              <li>
-                <Link to="/shirts">Shirts</Link>
-              </li>
-              <li>
-                <Link to="/caps">Caps</Link>
-              </li>
-            </ul>
-          )}
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/vendors">Vendors</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-
-      {/* Search Bar */}
-      <div className={styles.searchBar}>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleSearchChange}
-          placeholder="Search..."
-          className={styles.searchInput}
-        />
-        <FaSearch className={styles.searchIcon} />
-      </div>
-
-      {/* Icons Section */}
-      <div className={styles.icons}>
-        <Link to="/favorites" className={styles.iconWrapper}>
-          <FaHeart className={styles.icon} />
-          <span className={styles.iconBadge}>0</span>
-        </Link>
-        <Link to="/cart" className={styles.iconWrapper}>
-          <FaShoppingCart className={styles.icon} />
-          {cartCount > 0 && <span className={styles.iconBadge}>{cartCount}</span>}
-        </Link>
-        <div className={styles.profileDropdown}>
-          <FaUserCircle
-            className={styles.icon}
-            onClick={toggleProfile}
+      {/* Top Section */}
+      <div className={styles.topSection}>
+        {/* Search Bar */}
+        <div className={styles.searchBar}>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            placeholder="Search..."
+            className={styles.searchInput}
           />
-          {profileOpen && (
-            <ul className={styles.profileDropdownMenu}>
-              <li>
-                <Link to="/favorites">Wishlist</Link>
-              </li>
-              <li>
-                <Link to="/cart">Cart</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          )}
+          <FaSearch className={styles.searchIcon} />
+        </div>
+
+        {/* Logo */}
+        <div className={styles.logo}>
+          <Link to="/">**LogoWebsite**</Link>
+        </div>
+
+        {/* Icons */}
+        <div className={styles.icons}>
+          <Link to="/favorites" className={styles.iconWrapper}>
+            <FaHeart className={styles.icon} />
+            <span className={styles.iconBadge}>0</span>
+          </Link>
+          <Link to="/cart" className={styles.iconWrapper}>
+            <FaShoppingCart className={styles.icon} />
+            {cartCount > 0 && <span className={styles.iconBadge}>{cartCount}</span>}
+          </Link>
+          <div className={styles.profileDropdown}>
+            <FaUserCircle
+              className={`${styles.icon} ${profileOpen ? styles.active : ""}`}
+              onClick={toggleProfile}
+            />
+            {profileOpen && (
+              <ul className={styles.profileDropdownMenu}>
+                <li>
+                  <Link to="/favorites">Wishlist</Link>
+                </li>
+                <li>
+                  <Link to="/cart">Cart</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
 
-      {/* Hamburger Menu for Mobile */}
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
+      {/* Navigation Links */}
+      <div className={styles.navLinksContainer}>
+        <ul className={styles.navLinks}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/aboutus">About</Link>
+          </li>
+          <li className={`${styles.dropdown} ${collectionsOpen ? styles.active : ""}`}>
+            <Link
+              to="#"
+              className={styles.dropdownToggle}
+              onClick={(e) => {
+                e.preventDefault();
+                toggleCollections();
+              }}
+            >
+              Collections
+            </Link>
+            {collectionsOpen && (
+              <ul className={styles.dropdownMenu}>
+                <li>
+                  <Link to="/shirts">Shirts</Link>
+                </li>
+                <li>
+                  <Link to="/caps">Caps</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/vendors">Vendors</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
